281645556333ae452c780ccdd5e1225a48462191
Shreyas singh <71962705+shreyassingh96@users.noreply.github.com>
2023-04-12 23:18:35 -0400

    Added console app

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UtilityLibraries;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "Hello, World!";
+            bool startsWithUpper = input.StartsWithUpper();
+            Console.WriteLine($"The input starts with an uppercase letter: {startsWithUpper}");
+        }
+
+    }
+}
